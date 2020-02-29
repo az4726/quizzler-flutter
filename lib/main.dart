27a16685ce0dd8fc3244141c10f275f01496ad40
Azhar Zaman <azhar.zaman99@gmail.com>
2020-02-29 14:45:52 +0000

    Add functionality to display questions

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,15 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> ScoreKeeper = [];
+  List<Icon> scoreKeeper = [];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+
+  int questionNum = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNum],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -64,6 +72,11 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                if (questionNum < questions.length) {
+                  setState(() {
+                    questionNum++;
+                  });
+                }
               },
             ),
           ),
@@ -82,13 +95,18 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                if (questionNum < questions.length) {
+                  setState(() {
+                    questionNum++;
+                  });
+                }
               },
             ),
           ),
         ),
         //TODO: Add a Row here as your score keeper
         Row(
-          children: ScoreKeeper,
+          children: scoreKeeper,
         )
       ],
     );
